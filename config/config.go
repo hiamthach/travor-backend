2a755234c8167fc6ed113344781e8536dc517e4b
Ngoc Thach <thach.le@topebox.com>
2023-06-08 17:21:55 +0700

    #1 folder structure, project setup

--- config/config.go
+++ config/config.go
@@ -1,1 +1,33 @@
 package config
+
+import (
+	"github.com/spf13/viper"
+
+	"time"
+)
+
+type Config struct {
+	DBDriver             string        `mapstructure:"DB_DRIVER" env:"DB_DRIVER"`
+	DBSource             string        `mapstructure:"DB_SOURCE" env:"DB_SOURCE"`
+	ServerAddress        string        `mapstructure:"SERVER_ADDRESS" env:"SERVER_ADDRESS"`
+	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY" env:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION" env:"TOKEN_SYMMETRIC_KEY"`
+	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION" env:"REFRESH_TOKEN_DURATION"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/travor-backend/config"
+	"github.com/travor-backend/router"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Can not load config: ", err)
+	}
+
+	server, err := router.NewServer(config)
+	if err != nil {
+		log.Fatal("Can not create server: ", err)
+	}
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("Can not start server: ", err)
+	}
 }
