4f044a7cd0fe519431c7ee39a8b71d03244595bf
Ngoc Thach <thach.le@topebox.com>
2023-06-15 10:31:49 +0700

    db migration for reviews, blogs

---
+++ db/migration/000002_update_schema.down.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "blogs" DROP COLUMN "updated_at";
+
+ALTER TABLE "reviews" DROP COLUMN "updated_at";
+ALTER TABLE "reviews" DROP COLUMN "created_at";
+
+ALTER TABLE "reviews" DROP COLUMN "p_id";
+
+
