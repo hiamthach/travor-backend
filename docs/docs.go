1c59e4eded10f37837100236b5d38c02ea3816ae
Ngoc Thach <thach.le@topebox.com>
2023-06-20 15:33:35 +0700

    swagger init

--- docs/docs.go
+++ docs/docs.go
@@ -1729,7 +1729,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8088",
+	Host:             "",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Travor Backend API",
